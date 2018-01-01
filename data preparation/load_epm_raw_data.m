894c57c3d15ca5cb2fc40deb8593b71fc1459ddd
glad4enkonm <glad4enkonm@gmail.com>
2018-01-02 00:36:19 +0100

    Data preparation
    load_epm_raw_data added

---
+++ data preparation/load_epm_raw_data.m
@@ -0,0 +1,26 @@
+function allData = load_epm_raw_data(studentIds, dataSetPath)
+	%# loads cell array with specific ids
+	%# dataSetPath - path to folder with sessions
+	
+	sessionPathTemplate = [dataSetPath '/Session x/'];
+	sessions = 1:6;
+	
+	allData = [];
+	
+	for studentId = studentIds
+		
+		for session = sessions
+			pathToSessionFolder = strrep(sessionPathTemplate, "x",  mat2str(session));
+			pathToSessionForStudent = [pathToSessionFolder mat2str(studentId)];
+			disp(pathToSessionForStudent);
+			try
+				thisSessionRawData = read_mixed_csv(pathToSessionForStudent,',');
+				disp("ok");
+			catch
+				thisSessionRawData = [];
+				disp("error");
+			end
+			allData = [allData; thisSessionRawData];
+		endfor
+	endfor
+end
